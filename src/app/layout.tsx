0b3e5d3414aa75bfaa84bacebc76628a0cd9b5d1
eduard0vieira <eduardohvieira05@gmail.com>
2025-08-26 01:24:23 -0300

    ✨ feat: Implement login error handling and add Toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,38 @@
-import SideBar from '@/components/Sidebar/SideBar'
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import './globals.css'
+import SideBar from "@/components/Sidebar/SideBar";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ToastProvider } from "@/components/Ui/ToastProvider";
 
 const luckyBones = localFont({
-  src: './assets/fonts/LuckyBones-Bold.otf',
-  variable: '--font-lucky-bones',
-  weight: '700',
-  style: 'normal',
-  display: 'swap',
-})
+  src: "./assets/fonts/LuckyBones-Bold.otf",
+  variable: "--font-lucky-bones",
+  weight: "700",
+  style: "normal",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: 'Candyland',
-  description: 'Thought and Code by @jmorangus',
-}
+  title: "Candyland",
+  description: "Thought and Code by @jmorangus",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className={luckyBones.variable}>
       <body className={`${luckyBones.variable} antialiased font-sans`}>
-        <div className="flex">
-          <SideBar />
-          <main className="flex-1 transition-all duration-300 md:ml-64">
-            {children}
-          </main>
-        </div>
+        <ToastProvider />
+          <div className="flex">
+            <SideBar />
+            <main className="flex-1 transition-all duration-300 md:ml-64">
+              {children}
+            </main>
+          </div>
       </body>
     </html>
-  )
+  );
 }
