3d1727e8251c3b1c4052088d850fded2ae8b278a
Gabriel Pauletti Egea <ga.paulettiegea@gmail.com>
2025-09-08 14:48:19 -0300

    ✨ Refactor Header component to use CartButton and add AddToCartButton component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
+import CartButton from '../Buttons/CartBtn';
 import CandyLogo from '../icons/CandyLogo';
-import CartIcon from '../icons/header/CartIcon';
 import Search from './Search';
 
 export default function Header() {
@@ -7,9 +7,7 @@ export default function Header() {
     <div className="w-full h-max flex justify-between items-center">
       <CandyLogo />
       <Search />
-      <figure className="bg-pinkStrong w-10 max-sm:w-7 h-full p-2 max-sm:p-1.5 rounded-xl max-sm:rounded-lg ml-4 text-white">
-        <CartIcon />
-      </figure>
+      <CartButton />
     </div>
   );
 }
