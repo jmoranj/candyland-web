b86f96db1462bffc8afb679a561bdb184dec10cb
eduard0vieira <eduardohvieira05@gmail.com>
2025-10-09 20:35:31 -0300

    feat: AdminSideBar created, pages structure changes
    Signed-off-by: eduard0vieira <eduardohvieira05@gmail.com>

---
+++ src/app/admin/pedidos/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Pedidos() {
+  return (
+    <div>
+      <h1>Pedidos</h1>
+    </div>
+  );
+}
---
+++ src/app/admin/produtos/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Pedidos() {
+  return (
+    <div>
+      <h1>Criar Produtos</h1>
+    </div>
+  );
+}
