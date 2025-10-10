ae3118d227b9b15e2bc91eac57f2bc7803f553e0
jmoranj <morandim.joao@gmail.com>
2025-10-10 00:48:57 -0300

    feat: implement JWT authentication middleware and add contact page
    - Introduced middleware for handling authentication and route protection using JWT.
    - Added a new contact page component for user inquiries.
    - Updated ProductCard image source for consistency.

---
+++ src/middleware.ts
@@ -0,0 +1,81 @@
+// middleware.ts
+import { NextRequest, NextResponse } from "next/server";
+import { jwtDecode } from "jwt-decode";
+
+const AUTH_COOKIE_NAME = "authToken";
+
+// Use regex para rotas dinâmicas
+const publicRoutes = [
+  {
+    pattern: /^\/$/,
+    whenAuthenticated: "next"
+  },
+  {
+    pattern: /^\/login$/,
+    whenAuthenticated: "redirect"
+  },
+  {
+    pattern: /^\/pedidos$/,
+    whenAuthenticated: "redirect"
+  },
+  {
+    pattern: /^\/contato$/,
+    whenAuthenticated: "redirect"
+  },
+] as const;
+
+const privateRoutes = [
+  {
+    pattern: /^\/admin(\/.*)?$/,
+    whenAuthenticated: "next",
+    necessaryRole: "ADMIN"
+  },
+] as const;
+
+const REDIRECT_WHEN_NOT_AUTHENTICATED = "/login";
+
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const publicRoute = publicRoutes.find((route) => route.pattern.test(pathname));
+  const privateRoute = privateRoutes.find((route) => route.pattern.test(pathname));
+  const authToken = request.cookies.get(AUTH_COOKIE_NAME);
+
+  // Não autenticado e rota pública
+  if (!authToken && publicRoute) {
+    return NextResponse.next();
+  }
+
+  // Não autenticado e rota privada
+  if (!authToken && !publicRoute) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED;
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  // Autenticado e rota pública que redireciona
+  if (authToken && publicRoute && publicRoute.whenAuthenticated === "redirect") {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = "/";
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  // Autenticado e rota privada
+  if (authToken && privateRoute) {
+    const token = request.cookies.get(AUTH_COOKIE_NAME)?.value as string;
+    const decodedToken = jwtDecode(token);
+
+    if (decodedToken.exp && decodedToken.exp < Date.now() / 1000) {
+      const redirectUrl = request.nextUrl.clone();
+      redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED;
+      return NextResponse.redirect(redirectUrl);
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:jpg|jpeg|png|gif|svg|webp|ico)).*)"
+  ],
+};
